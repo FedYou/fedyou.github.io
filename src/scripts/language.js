ff83c47900a928df7c7142787e33ec77d0648c12
FedYou <federicochurruarin0@gmail.com>
2024-08-27 23:32:33 -0300

    feat: :sparkles: Added User Language Detection

--- src/scripts/language.js
+++ src/scripts/language.js
@@ -1,8 +1,11 @@
 import es from "../lang/es.json" assert { type: "json" };
 import en from "../lang/en.json" assert { type: "json" };
+const languageList = { es, en };
+const language = (window.navigator.language || navigator.browserLanguage).split(
+  "-"
+)[0];
 const elements = document.querySelectorAll("[key-lang]");
-
 elements.forEach((element) => {
   const key = element.getAttribute("key-lang");
-  element.textContent = es[key] ?? en[key];
+  element.textContent = languageList[language][key] ?? en[key];
 });
